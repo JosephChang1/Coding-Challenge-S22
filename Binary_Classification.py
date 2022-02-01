286de8172639bbdbb7dace8db0f0633c3ae87b9c
Joseph C <84756918+JosephChang1@users.noreply.github.com>
2022-02-01 01:32:09 -0600

    Update Binary_Classification.py

--- Binary_Classification.py
+++ Binary_Classification.py
@@ -4,10 +4,6 @@ Created on Mon Jan 31 22:48:29 2022
 
 @author: josep
 """
-import sys
-import numpy as np 
-np.set_printoptions(threshold=sys.maxsize)
-
 import pandas as pd 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OrdinalEncoder, LabelEncoder
@@ -18,46 +14,48 @@ from sklearn.metrics import accuracy_score
 
 #load data
 dataset  = pd.read_csv('mushrooms.csv')
-
-pd.set_option('display.max_columns', 30)
-
+#pd.set_option('display.max_columns', 30)
 X = dataset.iloc[:, 1:].values
 y = dataset.iloc[:, 0].values
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=1)
+# train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=21)
+#print('Train', X_train.shape, y_train.shape)
+#print('Test', X_test.shape, y_test.shape)
 
-print('Train', X_train.shape, y_train.shape)
-print('Test', X_test.shape, y_test.shape)
-
+# prepare input data, encode variables into integers
 oe = OrdinalEncoder()
 oe.fit(X_train)
 X_train_enc = oe.transform(X_train)
 X_test_enc = oe.transform(X_test)
 
-
+# prepare target variable, encode target variables into integers (0 or 1)
 le = LabelEncoder()
 le.fit(y_train)
 y_train_enc = le.transform(y_train)
 y_test_enc = le.transform(y_test)
 
-    
+# feature selection
 fs = SelectKBest(score_func=chi2, k='all')
 fs.fit(X_train_enc, y_train_enc)
 X_train_fs = fs.transform(X_train_enc)
 X_test_fs = fs.transform(X_test_enc)
-
 for i in range(len(fs.scores_)):
 	print('Feature %d: %f' % (i, fs.scores_[i]))
+    
 # plot the scores
 plt.bar([i for i in range(len(fs.scores_))], fs.scores_)
 plt.show()
 
+# fit the model
 model = LogisticRegression(solver='lbfgs', max_iter=10000)
 model.fit(X_train_enc, y_train_enc)
+
 # evaluate the model
 yhat = model.predict(X_test_enc)
+
 # evaluate predictions
 accuracy = accuracy_score(y_test_enc, yhat)
 print('Accuracy: %.2f' % (accuracy*100))
-
-print(y_test_enc)
+#print(yhat)
+#print(y_test_enc)
